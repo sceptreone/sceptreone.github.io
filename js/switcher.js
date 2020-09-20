ab1311ebbaec52855ed738d0b9a0193b52f2b7de
Mujtaba Shamas <msaneoup59@gmail.com>
2020-09-20 20:40:53 +0500

    Initial commit

---
+++ js/switcher.js
@@ -0,0 +1,68 @@
+/*-----------------------------------------------------------------------------------
+/* Styles Switcher
+-----------------------------------------------------------------------------------*/
+
+window.console = window.console || (function(){
+	var c = {}; c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile = c.clear = c.exception = c.trace = c.assert = function(){};
+	return c;
+})();
+
+
+jQuery(document).ready(function($) {
+	
+
+		$("ul.pattern .default" ).click(function(){
+			$("#color-opt").attr("href", "css/colors/default.css" );
+			return false;
+		});
+		
+		$("ul.pattern .blue" ).click(function(){
+			$("#color-opt").attr("href", "css/colors/blue.css" );
+			return false;
+		});
+		
+		$("ul.pattern .light-green" ).click(function(){
+			$("#color-opt").attr("href", "css/colors/light-green.css" );
+			return false;
+		});
+
+		$("ul.pattern .yellow" ).click(function(){
+			$("#color-opt").attr("href", "css/colors/yellow.css" );
+			return false;
+		});
+
+		$("ul.pattern .light-yellow" ).click(function(){
+			$("#color-opt").attr("href", "css/colors/light-yellow.css" );
+			return false;
+		});
+
+		$("ul.pattern .purple" ).click(function(){
+			$("#color-opt").attr("href", "css/colors/purple.css" );
+			return false;
+		});
+		
+		$("#style-switcher .bottom a.settings").click(function(e){
+			e.preventDefault();
+			var div = $("#style-switcher");
+			if (div.css("left") === "-60px") {
+				$("#style-switcher").animate({
+					left: "0px"
+				}); 
+			} else {
+				$("#style-switcher").animate({
+					left: "-60px"
+				});
+			}
+		})
+		
+		$("ul.pattern li a").click(function(e){
+			e.preventDefault();
+			$(this).parent().parent().find("a").removeClass("active");
+			$(this).addClass("active");
+		})
+
+		$("#style-switcher").animate({
+			left: "-60px"
+		}); 
+				
+	});
